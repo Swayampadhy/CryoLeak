71b904711de12824d308e580c8a977fe206f29a0
Swayam Tejas Padhy <37104162+Swayampadhy@users.noreply.github.com>
2023-08-10 12:21:51 -0700

    Initial push. Need to solve Apimodels namespace error.

---
+++ Agent/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Agent
+{
+    public static class Utilities
+    {
+        public static bool Is64Bit
+            => IntPtr.Size == 8;
+    }
+}
